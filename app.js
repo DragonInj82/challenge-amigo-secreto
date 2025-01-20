c0ad1e42c24912f68bfc84dde4b2a574e2b72a88
Pablo Perez <pabperezch@gmail.com>
2025-01-20 11:31:55 -0300

    generacion rama de desarrollo, actualiza el javascript

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+//generar una lista vacia para almacenar los amigos ingresados
+let amigos = [];
+
+//Implementa una función para agregar amigos
