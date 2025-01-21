e9d85c59c67c567e51d0c63c562868ab0f507a96
Pablo Perez <pabperezch@gmail.com>
2025-01-21 11:58:23 -0300

    Actualiza el JS, añade funcion agregarlista de amigos

--- app.js
+++ app.js
@@ -2,30 +2,38 @@
 Aquí deberás desarrollar la lógica para resolver el problema.
 */
 
+//Implementa una función para agregar amigos
 //generar una lista vacia para almacenar los amigos ingresados
 let amigos = [];
 
-//Implementa una función para agregar amigos
-
 //funcion para capturar texto usuario
-function agregarAmigo() {
-    let agregaAmigo = document.getElementById('amigo').value; //obtiene el texto ingresado por usuario con id 'amigo'
-    //si el campo no esta vacio, agregar al final de la lista
-    if(agregaAmigo != '') {
-        amigos.push(agregaAmigo);        
-    //de lo contrario, emitir una alerta para ingresar un nombre
-    } else {
-        alert('Por favor, inserte un nombre');
-    }
-    //funcion para limpiar el campo de entrada una vez ingresado el nombre a la lista
-    limpiarCaja();
-    //reornar la lista
-    return console.log(amigos);
+ function agregarAmigo() {
+     let agregaAmigo = document.getElementById('amigo').value; //obtiene el texto ingresado por usuario con id 'amigo'
+     agregarLista(agregaAmigo);
+     //si el campo no esta vacio, agregar al final de la lista
+     if(agregaAmigo == '') {
+         alert('Por favor, inserte un nombre');    
+     //de lo contrario, emitir una alerta para ingresar un nombre
+     } else {
+         amigos.push(agregaAmigo);         
+     }
+     //funcion para limpiar el campo de entrada una vez ingresado el nombre a la lista
+     limpiarCaja();
+           
+ }
+ // funcion limpiar campo de texto
+ function limpiarCaja() {
+     document.querySelector('#amigo').value = '';
+ }
+
+//Implementa una función para actualizar la lista de amigos 
+function agregarLista(texto) {
+    let elementoHTML = document.getElementById("listaAmigos");
+        elementoHTML.innerHTML += `<li> ${texto} </li>`;
 }
-// funcion limpiar campo de texto
-function limpiarCaja() {
-    document.querySelector('#amigo').value = '';
-}
+
+
+
 
 
 
